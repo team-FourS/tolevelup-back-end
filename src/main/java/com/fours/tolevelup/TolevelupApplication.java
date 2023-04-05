048e6789c6d630e76956ae41cb28a1dd6b1082b5
siwon <muncool39@gmail.com>
2023-04-05 16:31:43 +0900

    first commit

---
+++ src/main/java/com/fours/tolevelup/TolevelupApplication.java
@@ -0,0 +1,13 @@
+package com.fours.tolevelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolevelupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolevelupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fours/tolevelup/TolevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fours.tolevelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolevelupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
