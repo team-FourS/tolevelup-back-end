d7372afed31c4d658d3c0c0ff732d5a338a37d7c
SeoYeon Min <tjdus553926@gmail.com>
2023-04-09 13:28:21 +0900

    friend패키지 안 클래스들 생성

--- src/main/java/com/fours/tolevelup/friend/Friend.java
+++ src/main/java/com/fours/tolevelup/friend/Friend.java
@@ -1,6 +1,20 @@
 package com.fours.tolevelup.friend;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class Friend {
+
+    @Id
+    private int id;
+    private String friend_character_name;
+    private long exp;
 }
 
-// pull 테스트용
