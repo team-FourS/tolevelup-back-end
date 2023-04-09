253d398b915e8834fd3921b54181632bc3d4f491
SeoYeon Min <tjdus553926@gmail.com>
2023-04-09 14:17:06 +0900

    FriendController 수정_2

--- src/main/java/com/fours/tolevelup/friend/Friend.java
+++ src/main/java/com/fours/tolevelup/friend/Friend.java
@@ -1,2 +1,17 @@
-package com.fours.tolevelup.Friend;public class Friend {
+package com.fours.tolevelup.friend;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Friend {
+    @Id
+    private int id;
+    private String friend_character_name;
+    private long exp;
 }
