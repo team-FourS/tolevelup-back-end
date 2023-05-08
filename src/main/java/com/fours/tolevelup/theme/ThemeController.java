6c187eb34e3ba7bc3db511ce93c1271128e9d857
siwon <muncool39@gmail.com>
2023-05-08 14:11:44 +0900

    ThemeService+DI

--- src/main/java/com/fours/tolevelup/theme/ThemeController.java
+++ src/main/java/com/fours/tolevelup/theme/ThemeController.java
@@ -1,9 +1,16 @@
 package com.fours.tolevelup.theme;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ThemeController {
+
+    final private ThemeServiceImpl themeService;
+    @Autowired
+    public ThemeController(ThemeServiceImpl themeService){
+        this.themeService = themeService;
+    }
 
 }
---
+++ src/main/java/com/fours/tolevelup/theme/ThemeService.java
@@ -0,0 +1,5 @@
+package com.fours.tolevelup.theme;
+
+public interface ThemeService {
+
+}
