650ceb19177074c7c9424bc0dbf9b0f7deb8c22f
SeoYeon Min <tjdus553926@gmail.com>
2023-05-21 22:29:32 +0900

    missionLog repository

--- src/main/java/com/fours/tolevelup/missionlog/MissionLogRepository.java
+++ src/main/java/com/fours/tolevelup/missionlog/MissionLogRepository.java
@@ -2,9 +2,13 @@ package com.fours.tolevelup.missionlog;
 
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+
 @Repository
 public interface MissionLogRepository {
     MissionLog findById(String user_id);
     void save(MissionLog missionLog);
+    void missionChecked(Timestamp end_date, String status);
+    MissionLog findByMissionId(int mission_id);
 
 }
--- src/main/java/com/fours/tolevelup/missionlog/MissionLogRepositoryImpl.java
+++ src/main/java/com/fours/tolevelup/missionlog/MissionLogRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.fours.tolevelup.missionlog;
 
 import com.fours.tolevelup.mission.MissionRepositoryImpl;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.EntityManager;
+import java.sql.Timestamp;
 
 public class MissionLogRepositoryImpl implements MissionLogRepository{
 
@@ -19,6 +23,17 @@ public class MissionLogRepositoryImpl implements MissionLogRepository{
     @Override
     public void save(MissionLog missionLog){
         em.persist(missionLog);
+    }
+
+    @Override
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE MissionLog m set m.end_date = :end_date, m.status = 'checked'")
+    public void missionChecked(@Param("end_date")Timestamp end_date, @Param("status") String status)
+    {}
+
+    @Override
+    public MissionLog findByMissionId(int mission_id){
+        return em.find(MissionLog.class, mission_id);
     }
 
 
