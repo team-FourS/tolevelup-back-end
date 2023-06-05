ad2c216dde9fcf1e54279860e18b76a3019d594f
siwon <muncool39@gmail.com>
2023-06-06 04:57:49 +0900

    themeRepo param추가

--- src/main/java/com/fours/tolevelup/themeexp/ThemeExpRepository.java
+++ src/main/java/com/fours/tolevelup/themeexp/ThemeExpRepository.java
@@ -3,14 +3,15 @@ package com.fours.tolevelup.themeexp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ThemeExpRepository extends JpaRepository<ThemeExp, Long>, ThemeExpCustomRepository {
 
     @Modifying(clearAutomatically = true)
     @Query("update ThemeExp t set t.exp_total = t.exp_total + :exp_total where t.user.id = :uid and t.theme.id = :tid")
-    int updateExpPlus(float exp_total, String user_id, int theme_id);
+    int updateExpPlus(@Param("exp_total") float exp_total,@Param("uid") String user_id,@Param("tid") int theme_id);
 
     @Modifying(clearAutomatically = true)
     @Query("update ThemeExp t set t.exp_total = t.exp_total - :exp_total where t.user.id = :uid and t.theme.id = :tid")
-    int updateExpMinus(float exp_total, String user_id, int theme_id);
+    int updateExpMinus(@Param("exp_total") float exp_total,@Param("uid") String user_id,@Param("tid") int theme_id);
 }
