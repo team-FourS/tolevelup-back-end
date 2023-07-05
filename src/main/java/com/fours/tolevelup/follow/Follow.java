6ecd5d780957ed5df16c3322de2d7f7eafc95b0b
siwon <muncool39@gmail.com>
2023-07-05 16:52:45 +0900

    follow 패키지 class 추가

--- src/main/java/com/fours/tolevelup/follow/Follow.java
+++ src/main/java/com/fours/tolevelup/follow/Follow.java
@@ -1,4 +1,4 @@
-package com.fours.tolevelup.user;
+package com.fours.tolevelup.follow;
 
 import lombok.Builder;
 import lombok.Getter;
---
+++ src/main/java/com/fours/tolevelup/follow/FollowRepository.java
@@ -0,0 +1,4 @@
+package com.fours.tolevelup.follow;
+
+public class FollowRepository {
+}
