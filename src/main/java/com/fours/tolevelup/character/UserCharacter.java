f6b47bd4a83a95e64b2d5e40f1c67bd49f3ba569
SeoYeon Min <tjdus553926@gmail.com>
2023-07-12 01:44:31 +0900

    UserCharacter 엔티티

--- src/main/java/com/fours/tolevelup/character/UserCharacter.java
+++ src/main/java/com/fours/tolevelup/character/UserCharacter.java
@@ -1,4 +1,38 @@
 package com.fours.tolevelup.character;
 
+import com.fours.tolevelup.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class UserCharacter {
+    @Id
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "character_id")
+    private Character character;
+
+    private String character_name;
+
+    @Builder
+    public UserCharacter(int id, User user, Character character, String character_name){
+        this.id = id;
+        this.user = user;
+        this.character = character;
+        this.character_name = character_name;
+    }
+
 }
