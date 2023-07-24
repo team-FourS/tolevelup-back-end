431796bfa3ec155fa168ef6c725d8c8400cc42c3
SeoYeon Min <tjdus553926@gmail.com>
2023-07-24 18:22:45 +0900

    CharacterDTO 파일에 Reqeustbody 어노테이션 추가

--- src/main/java/com/fours/tolevelup/configuration/AuthenticationConfig.java
+++ src/main/java/com/fours/tolevelup/configuration/AuthenticationConfig.java
@@ -1,7 +1,6 @@
 package com.fours.tolevelup.configuration;
 
 
-import io.jsonwebtoken.Jwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
