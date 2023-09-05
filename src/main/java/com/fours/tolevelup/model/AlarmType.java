f1c8d74ece987aaf39172c61d422d3feb66df9e0
siwon <muncool39@gmail.com>
2023-09-05 17:39:35 +0900

    Add entity alarm comment like
    기능 추가를 위한 엔티티 틀

---
+++ src/main/java/com/fours/tolevelup/model/AlarmType.java
@@ -0,0 +1,9 @@
+package com.fours.tolevelup.model;
+
+import lombok.Getter;
+
+@Getter
+public enum AlarmType {
+    NEW_LIKE,
+    NEW_COMMENT
+}
---
+++ src/main/java/com/fours/tolevelup/model/entity/Like.java
@@ -0,0 +1,36 @@
+package com.fours.tolevelup.model.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "other_id")
+    private User other_user;
+
+    @Column(name = "feed_date")
+    private Date date;
+
+    @Column(name = "register_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+}
