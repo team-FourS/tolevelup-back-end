ac0e89a25e337c65699c00c9d1feceab1ef1676b
SeoYeon Min <tjdus553926@gmail.com>
2023-09-30 00:41:32 +0900

    Test: 캐릭터 이미지 경로 리턴하는 api

--- src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
+++ src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
@@ -1,15 +1,23 @@
 package com.fours.tolevelup.controller.api;
 
 
-import com.fours.tolevelup.controller.response.Response;
 import com.fours.tolevelup.model.entity.Character;
 import com.fours.tolevelup.service.character.CharacterDTO;
 import com.fours.tolevelup.service.character.CharacterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -36,6 +44,23 @@ public class CharacterController {
     @GetMapping("/userCharacter")
     public ResponseEntity<List<CharacterDTO.UserCharacter>> userCharacterData(Authentication authentication){
         return ResponseEntity.ok(characterService.getUserCharacterData(authentication.getName()));
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<Resource> returnImage(@RequestParam String imageName){
+        String path = "src/main/resources/img/";
+        Resource resource = new FileSystemResource(path + imageName);
+
+        HttpHeaders header = new HttpHeaders();
+        Path filePath = null;
+
+         try{
+             filePath = Paths.get(path + imageName);
+             header.add("Content-Type", Files.probeContentType(filePath));
+         } catch (IOException e){
+             e.printStackTrace();
+         }
+        return new ResponseEntity<Resource>(resource,header, HttpStatus.OK);
     }
 
 
