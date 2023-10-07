7efaf84dd32322efb7d3217fcf5c724d139a9872
siwon <muncool39@gmail.com>
2023-10-07 17:31:20 +0900

    add: 피드에 보내진 코멘트 데이터용 dto 클래스 추가

--- src/main/java/com/fours/tolevelup/model/FeedDTO.java
+++ src/main/java/com/fours/tolevelup/model/FeedDTO.java
@@ -5,6 +5,7 @@ import com.fours.tolevelup.model.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 import java.sql.Date;
 import java.sql.Timestamp;
@@ -20,6 +21,27 @@ public class FeedDTO {
     public static class feedData{
         private UserDTO.publicUserData userData;
         private List<MissionDTO.mission> userCompleteMissions;
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class FeedComments{
+        private Long commentId;
+        private UserDTO.publicUserData fromUserData;
+        private String comment;
+        private Timestamp registeredAt;
+        private Timestamp updatedAt;
+
+        public static FeedComments fromComment(Comment comment){
+            return new FeedComments(
+                    comment.getId(),
+                    UserDTO.publicUserData.fromUser(comment.getFromUser()),
+                    comment.getComment(),
+                    comment.getRegisteredAt(),
+                    comment.getUpdatedAt()
+            );
+        }
     }
 
     @Getter
