dc82d5a87d06dc407fcd64bf17d6364a0ab7f1da
siwon <muncool39@gmail.com>
2023-10-10 19:11:50 +0900

    add: 테마별 카운트를 전달하기 위한 dto와 response

--- src/main/java/com/fours/tolevelup/controller/response/StatsResponse.java
+++ src/main/java/com/fours/tolevelup/controller/response/StatsResponse.java
@@ -1,2 +1,25 @@
-package com.fours.tolevelup.controller.response;public class StatsResponse {
+package com.fours.tolevelup.controller.response;
+
+
+import com.fours.tolevelup.model.MissionDTO;
+import com.fours.tolevelup.model.StatsDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StatsResponse {
+
+    @Getter
+    @AllArgsConstructor
+    public static class ThemeCounts{
+        String theme;
+        long completeCounts;
+        public static ThemeCounts fromDTO(StatsDTO.ThemeCompleteCounts counts){
+            return new ThemeCounts(
+                    counts.getThemeName(),
+                    counts.getCompleteCounts()
+            );
+        }
+    }
 }
--- src/main/java/com/fours/tolevelup/model/StatsDTO.java
+++ src/main/java/com/fours/tolevelup/model/StatsDTO.java
@@ -1,2 +1,19 @@
-package com.fours.tolevelup.model;public class StatsDTO {
+package com.fours.tolevelup.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StatsDTO {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ThemeCompleteCounts{
+        String themeName;
+        long completeCounts;
+    }
 }
