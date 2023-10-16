db3e04aa65102d7ffedff01433def5178fd0ba5a
SeoYeon Min <tjdus553926@gmail.com>
2023-10-16 17:58:35 +0900

    Docs: 캐릭터 이미지 api 변경 코드

--- src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
+++ src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
@@ -6,7 +6,9 @@ import com.fours.tolevelup.service.character.CharacterDTO;
 import com.fours.tolevelup.service.character.CharacterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -51,24 +54,21 @@ public class CharacterController {
     }
 
     @GetMapping("/image")
-    public ResponseEntity<Resource> returnImage(@RequestParam String imageName){
-        String path = "src/main/resources/img/";
-        Resource resource = new FileSystemResource(path + imageName);
+    public ResponseEntity<Resource> returnImage(@RequestParam String imageName) {
+        String path = "img/" + imageName;
+        Resource resource = new ClassPathResource(path);
 
         HttpHeaders header = new HttpHeaders();
         Path filePath = null;
 
-         try{
-             filePath = Paths.get(path + imageName);
-             header.add("Content-Type", Files.probeContentType(filePath));
-         } catch (IOException e){
-             e.printStackTrace();
-         }
-
-        String absoluteImagePath = imageUrlBasePath + imageName;
-        header.add(HttpHeaders.LOCATION, absoluteImagePath);
-        return new ResponseEntity<Resource>(resource,header, HttpStatus.OK);
+        try {
+            InputStream inputStream = resource.getInputStream();
+            header.add("Content-Type", Files.probeContentType(Paths.get(resource.getURI())));
+            return new ResponseEntity<>(new InputStreamResource(inputStream), header, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-
 
 }
