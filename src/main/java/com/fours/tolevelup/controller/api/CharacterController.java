25cad54cfff78413dcd29d65efb24fedd5d7f8fc
gitseoyeon <tjdus553926@gmail.com>
2023-10-23 16:39:31 +0900

    Docs: 이미지 url 관련 어노테이션 삭제

--- src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
+++ src/main/java/com/fours/tolevelup/controller/api/CharacterController.java
@@ -34,10 +34,6 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
-    @Value("${image.base-url}")
-    private String imageUrlBasePath;
-
-
     @GetMapping("/character")
     public ResponseEntity<List<CharacterDTO.CharacterData>> characterData() {
         return ResponseEntity.ok(characterService.getCharacterData());
