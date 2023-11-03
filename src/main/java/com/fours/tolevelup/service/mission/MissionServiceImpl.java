241449f3f5862dd79ada209bb03aa450ea9c7c39
gitseoyeon <tjdus553926@gmail.com>
2023-11-03 18:11:25 +0900

    Docs: themeExp 오르는 메서드 다시 추가

--- src/main/java/com/fours/tolevelup/service/mission/MissionServiceImpl.java
+++ src/main/java/com/fours/tolevelup/service/mission/MissionServiceImpl.java
@@ -68,8 +68,9 @@ public class MissionServiceImpl implements MissionService {
         Mission mission = getMissionOrException(missionId);
         Date startDate = getStartDate(mission.getTheme().getType());
         MissionLog missionLog = getMissionLogOrException(user,mission,startDate);
-        int beforeExp = themeExpRepository.exp(user, mission.getTheme());
         missionLogRepository.updateMissionLogStatus(missionLog,changeStatus(missionLog),getEndTime(missionLog));
+        int beforeExp = themeExpRepository.exp(user, mission.getTheme());
+        themeExpRepository.updateExp(getMissionExp(missionLog), user, mission.getTheme());
         int afterExp = themeExpRepository.exp(user, mission.getTheme());
 
         if(afterExp % 10 > beforeExp % 10) {
